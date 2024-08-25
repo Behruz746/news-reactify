36b0c73535e6edb6f99b83b9da609ccf94d99feb
Behruz <madaminovbehruz746@gmail.com>
2024-08-25 15:43:55 +0500

    added: skeleton

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Header } from "./header/Header";
 export { default as NewsBanner } from "./news-banner/NewsBanner";
 export { default as NewsList } from "./news-list/NewsList";
+export { default as Skeleton } from "./skeleton/Skeleton";
 export { default as NewsItem } from "./news-item/NewsItem";
 export { default as Image } from "./image/Image";
--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,17 +1,19 @@
 import React, { useEffect, useState } from "react";
 import styles from "./styles.module.scss";
-import { NewsBanner, NewsList } from "../../components";
+import { NewsBanner, NewsList, Skeleton } from "../../components";
 import { getNews } from "../../service/news";
 
 function Main() {
   const [news, setNews] = useState([]);
+  const [load, setLoad] = useState(true);
 
   useEffect(() => {
     const fetchNews = async () => {
       try {
+        setLoad(true);
         const response = await getNews();
         setNews(response.data.news);
-        console.log(response.data.news);
+        setLoad(false);
       } catch (error) {
         console.log(error);
       }
@@ -24,8 +26,17 @@ function Main() {
     <>
       <main className={styles.main}>
         <div className={"container"}>
-          {news.length > 0 ? <NewsBanner item={news[13]} /> : null}
-          <NewsList news={news} />
+          {news.length > 0 && !load ? (
+            <NewsBanner item={news[0]} />
+          ) : (
+            <Skeleton count={1} type={"banner"} />
+          )}
+
+          {!load ? (
+            <NewsList news={news} />
+          ) : (
+            <Skeleton count={10} type={"item"} />
+          )}
         </div>
       </main>
     </>
